e2abdf81f2594150b6d6ac4f6e09e4843d07e4ba
Nasir Ahmad <G00419630@atu.ie>
2024-10-09 14:51:58 +0100

    updating code

--- src/main/java/com/example/week4cicid/Product.java
+++ src/main/java/com/example/week4cicid/Product.java
@@ -1,5 +1,17 @@
 package com.example.week4cicid;
 
-public class Product {
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Product {
+    @NotBlank (message ="Name cannot be blank")
+    private String name;
+
+    @Min(value=0, message="must be greater or equal to 1")
+    private int id;
 }
--- src/main/java/com/example/week4cicid/ProductController.java
+++ src/main/java/com/example/week4cicid/ProductController.java
@@ -1,4 +1,26 @@
 package com.example.week4cicid;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/product")
 public class ProductController {
+    List<Product> productList = new ArrayList<>();
+
+    @GetMapping("/getProducts")
+    public String getProducts(){
+        return "";
+    }
+
+    @PostMapping("/addProducts")
+    public List<Product> addProduct(@RequestBody Product product)
+    {
+        productList.add(product);
+        return productList;
+    }
+
+
 }
